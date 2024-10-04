fa65d43540458fd3693c92e954a447d786446b7c
steve192 <stefan@sterul.com>
2024-10-04 10:20:11 +0000

    Initial commit

---
+++ src/app.test.tsx
@@ -0,0 +1,20 @@
+import {
+  afterEach, beforeEach,
+  describe, expect, it, vi,
+} from 'vitest';
+// import { TestService } from './TestService';
+
+// const testMethodMock = vi.spyOn(TestService, 'testMethod');
+
+describe('Test', () => {
+  afterEach(() => {
+    vi.resetAllMocks();
+  });
+  beforeEach(() => {
+  });
+
+  it('Test something', async () => {
+
+  });
+
+});
